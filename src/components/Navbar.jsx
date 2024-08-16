ee423145568338063f1619c3493cf590460f9da4
mylittlecorner <139819354+p1tap@users.noreply.github.com>
2024-08-16 23:35:15 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import Navbar from 'react-bootstrap/Navbar';
 function AppNavbar() {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="/">My App</Navbar.Brand>
+      <Navbar.Brand href="/accessory-shop/routing-app">My App</Navbar.Brand>
       <Nav className="mr-auto">
         <Nav.Link as={Link} to="/accessory-shop/routing-app">Accessory Shop</Nav.Link>
         <Nav.Link as={Link} to="/accessory-shop/routing-app/about">Test Path</Nav.Link>
